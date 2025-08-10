ccd4e91360a9748e2427f31bdfd3245f7c94e246
Anamitra Upadhyay <128287013+anamitraupadhyay@users.noreply.github.com>
2025-08-10 07:06:56 +0000

    did some final changes in the exmle code to demostrate the doPost setup

--- examples/servlet-app/src/main/java/UserServlet.java
+++ examples/servlet-app/src/main/java/UserServlet.java
@@ -1,39 +1,26 @@
-//package main.java;
+import Quarklets.essentials.JsonUtils;
 
-import jakarta.servlet.ServletException;
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import main.java.InputPOJO;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import Quarklets.essentials.*;
-
 @WebServlet("/user")
-public class UserServlet extends BaseServlet {
-
-    /*@JsonBody
-    public static class UserRequest {
-        public String name;
-        public int age;
-    }*/
-
+public class UserServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        JsonObject json = JsonUtils.parse(req);
+        String name = json.getString("Name", "Unknown");
+        int age = json.getInt("age", 0);
 
-        /*
-         * UserRequest user = parseBody(req, UserRequest.class);
-         * writeJson(res, Map.of("message", "User added: " + user.name));
-         */
-        //InputPOJO obj = new InputPOJO().jsonparse(HttpServletRequest req);
-        /*BaseServlet obj = new ; cant find a wayout stuck and forgotten how to and not to make objects of classes and which classes permit here does extending httpservlet permit or not
-        UserServlet() obj = new UserServlet();*/
-        //after declaring static method to jsonparse and extending that i guess its best to just call the method and it will return and object will be stored in something but doesnt static method might fail idk what am asking right now, ok btw i return jsonreader object 
-        //JsonObject obj = jsonparse(req);
-        //JsonReader obj1 = jsonparse(req);
+        JsonObject responseJson = Json.createObjectBuilder()
+            .add("status", "success")
+            .add("name", name)
+            .add("age", age)
+            .build();
+
+        JsonUtils.write(res, responseJson);
     }
 }
